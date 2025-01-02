e7938baa5da718103b338d2ca792a5e3ab8fc95f
zuGROB <68923431+zuGROB@users.noreply.github.com>
2025-01-02 11:33:17 +0500

    Add files via upload

--- BH_CardSync.py
+++ BH_CardSync.py
@@ -8,9 +8,10 @@ import threading
 import time
 from datetime import datetime
 
-SERVER_ADDRESS = '255.255.255.255'  # типа адрес
+SERVER_ADDRESS = 'localhost'  # Или IP
 PORT = 1337
-FOLDER = os.path.abspath('C:\\KKS\\UserData\\chara\\female\\burning_hellas_2')
+CARD_FOLDER = os.path.abspath('C:\\KKS\\UserData\\chara\\female\\burning_hellas_2')  # Папка для карточек
+MOD_FOLDER = os.path.abspath('C:\\KKS\\mods')  # Папка для модов
 
 sock = None
 
@@ -19,112 +20,157 @@ def connect_to_server():
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((SERVER_ADDRESS, PORT))
-        status_label.config(text="Подключено к серверу!")
+        status_label.config(text="Connected to server")
         update_file_lists()
     except Exception as e:
-        messagebox.showerror("Error", f"Невозможно подключиться к серверу: {e}")
-
+        messagebox.showerror("Error", f"Не получилось подключиться к серверу: {e}")
 
 def disconnect_from_server():
     global sock
     if sock:
         sock.close()
         sock = None
-        status_label.config(text="Отключено.")
+        status_label.config(text="Отключено")
         clear_file_lists()
 
 def clear_file_lists():
-    for i in server_file_treeview.get_children():   # уох
-        server_file_treeview.delete(i)
-    for i in local_file_treeview.get_children():    #уох х2
-        local_file_treeview.delete(i)
+    for tree in [server_card_treeview, local_card_treeview, server_mod_treeview, local_mod_treeview]:
+        for i in tree.get_children():
+            tree.delete(i)
 
-def update_file_lists():
+def update_file_lists(folder_type="all"):
     clear_file_lists()
     if not sock:
         return
 
-    try:
-        sock.sendall(json.dumps({'command': 'list_files'}).encode())
-        response = sock.recv(8192).decode()
-
+    if folder_type in ("all", "cards"):
         try:
-            server_files = json.loads(response)
-            if 'error' in server_files:
-                raise Exception(server_files['error'])
+            sock.sendall(json.dumps({'command': 'list_files', 'folder': 'cards'}).encode())
+            response = sock.recv(8192).decode()
 
-            for filename, data in server_files.items():
-                mtime = datetime.fromtimestamp(data['mtime'])
-                mtime_str = mtime.strftime('%Y-%m-%d %H:%M:%S')
-                size_mb = round(data['size'] / (1024 * 1024), 2)  # Размер в МБ, nerd shit
-                server_file_treeview.insert("", tk.END, values=(filename, size_mb, mtime_str))
+            try:
+                server_files = json.loads(response)
+                if 'error' in server_files:
+                    raise Exception(server_files['error'])
 
-        except json.JSONDecodeError as e:
-            messagebox.showerror("Error", f"Ошибка загрузки с сервера: {e}")
-            return
+                for filename, data in server_files.items():
+                    mtime = datetime.fromtimestamp(data['mtime'])
+                    mtime_str = mtime.strftime('%Y-%m-%d %H:%M:%S')
+                    size_mb = round(data['size'] / (1024 * 1024), 2)
+                    server_card_treeview.insert("", tk.END, values=(filename, size_mb, mtime_str))
+
+            except json.JSONDecodeError as e:
+                messagebox.showerror("Error", f"Ошибка загрузки карточки: {e}")
+                return
+            except Exception as e:
+                messagebox.showerror("Error", f"Ошибка обработки данных: {e}")
+                return
+
+
+            for filename in os.listdir(CARD_FOLDER):
+                filepath = os.path.join(CARD_FOLDER, filename)
+                if os.path.isfile(filepath):
+                    size = os.path.getsize(filepath)
+                    mtime = os.path.getmtime(filepath)
+                    mtime_str = datetime.fromtimestamp(mtime).strftime('%Y-%m-%d %H:%M:%S')
+                    size_mb = round(size / (1024 * 1024), 2)
+                    local_card_treeview.insert("", tk.END, values=(filename, size_mb, mtime_str))
+
         except Exception as e:
-            messagebox.showerror("Error", f"Ошибка обработки данных: {e}")
-            return
-
-        for filename in os.listdir(FOLDER):
-            filepath = os.path.join(FOLDER, filename)
-            if os.path.isfile(filepath):
-                size = os.path.getsize(filepath)
-                mtime = os.path.getmtime(filepath)
-                mtime_str = datetime.fromtimestamp(mtime).strftime('%Y-%m-%d %H:%M:%S')
-                size_mb = round(size / (1024 * 1024), 2)
-                local_file_treeview.insert("", tk.END, values=(filename, size_mb, mtime_str))
-
-    except Exception as e:
-        messagebox.showerror("Error", f"Не получилось обновить списки файлов: {e}")
+            messagebox.showerror("Error", f"Не вышло обновить списки карт: {e}")
 
 
-                        # я ничерта не помню, что я тут написал.
-def download_selected():
+
+    if folder_type in ("all", "mods"):
+        try:
+            sock.sendall(json.dumps({'command': 'list_files', 'folder': 'mods'}).encode())
+            response = sock.recv(8192).decode()
+
+            try:
+                server_files = json.loads(response)
+                if 'error' in server_files:
+                    raise Exception(server_files['error'])
+
+                for filename, data in server_files.items():
+                    mtime = datetime.fromtimestamp(data['mtime'])
+                    mtime_str = mtime.strftime('%Y-%m-%d %H:%M:%S')
+                    size_mb = round(data['size'] / (1024 * 1024), 2)
+                    server_mod_treeview.insert("", tk.END, values=(filename, size_mb, mtime_str))
+
+            except json.JSONDecodeError as e:
+                messagebox.showerror("Error", f"Ошибка загрузки мода: {e}")
+                return
+            except Exception as e:
+                messagebox.showerror("Error", f"Ошибка обработки данных: {e}")
+                return
+
+
+            for filename in os.listdir(MOD_FOLDER):
+                filepath = os.path.join(MOD_FOLDER, filename)
+                if os.path.isfile(filepath):
+                    size = os.path.getsize(filepath)
+                    mtime = os.path.getmtime(filepath)
+                    mtime_str = datetime.fromtimestamp(mtime).strftime('%Y-%m-%d %H:%M:%S')
+                    size_mb = round(size / (1024 * 1024), 2)
+                    local_mod_treeview.insert("", tk.END, values=(filename, size_mb, mtime_str))
+
+        except Exception as e:
+            messagebox.showerror("Error", f"Ошибка обработки данных с сервера: {e}")
+
+
+#                       ctrl-c ctrl-v goes brrrrrrrr
+def download_selected(folder_type):
     if not sock:
         return
-    selected_items = server_file_treeview.selection()
+
+    treeview = server_card_treeview if folder_type == "cards" else server_mod_treeview
+    local_folder = CARD_FOLDER if folder_type == "cards" else MOD_FOLDER
+    selected_items = treeview.selection()
+
     for item in selected_items:
-        filename = server_file_treeview.item(item)['values'][0]
+        filename = treeview.item(item)['values'][0]
+
         try:
-            sock.sendall(json.dumps({'command': 'get_file', 'filename': filename}).encode())
+            sock.sendall(json.dumps({'command': 'get_file', 'filename': filename, 'folder': folder_type}).encode())
             filesize_bytes = sock.recv(1024).decode().strip()
             filesize = int(filesize_bytes)
-            filepath = os.path.join(FOLDER, filename)
+
+            filepath = os.path.join(local_folder, filename)
+
             with open(filepath, 'wb') as f:
                 received = 0
                 while received < filesize:
                     data = sock.recv(4096)
                     if not data:
-                        raise Exception("Загрузка файла прервана по хер его знает каким причинам.")
+                        raise Exception("Загрузка оборвалась")
                     f.write(data)
                     received += len(data)
+            messagebox.showinfo("Success", f"'{filename}' успешно загружен.")
+
 
         except Exception as e:
-            messagebox.showerror("Error", f"Could not download '{filename}': {e}")
-            continue  # продолжаем загрузку других файлов
+            messagebox.showerror("Error", f"Не вышло загрузить '{filename}': {e}")
 
-        messagebox.showinfo("Заебис)", f"'{filename}' успешно загружен.")
-
-
-    update_file_lists()  # обновляем списки после загрузки
+    update_file_lists(folder_type)
 
 
-def upload_selected():
+
+def upload_selected(folder_type):
     if not sock:
         return
 
-    selected_items = local_file_treeview.selection() #  Выбор из Treeview
-    if not selected_items:
+    folder = CARD_FOLDER if folder_type == "cards" else MOD_FOLDER
+    filepaths = filedialog.askopenfilenames(initialdir=folder)
+    if not filepaths:
         return
 
-    for item in selected_items:
-        filename = local_file_treeview.item(item)['values'][0]
-        filepath = os.path.join(FOLDER, filename) # Формируем полный путь
+    for filepath in filepaths:
+        filename = os.path.basename(filepath)
         filesize = os.path.getsize(filepath)
 
         try:
-            sock.sendall(json.dumps({'command': 'upload_file', 'filename': filename, 'size': filesize}).encode())
+            sock.sendall(json.dumps({'command': 'upload_file', 'filename': filename, 'size': filesize, 'folder': folder_type}).encode())
+
             with open(filepath, 'rb') as f:
                 while True:
                     data = f.read(4096)
@@ -132,83 +178,103 @@ def upload_selected():
                         break
                     sock.sendall(data)
 
-            messagebox.showinfo("Заебис)", f"'{filename}' успешно выгружен на сервер.")
+            messagebox.showinfo("Success", f"'{filename}' успешно выгружен на сервер.")
 
 
         except Exception as e:
-            messagebox.showerror("Error", f"Не получилось выгрузить на сервер '{filename}': {e}")
-            continue
+            messagebox.showerror("Error", f"Не вышло выгрузить '{filename}' на сервер: {e}")
 
-    update_file_lists()
+    update_file_lists(folder_type)
 
-# --- GUI ---
+
+# --- GUI ---               И ЭТО ПИЗЕЦ
 
 window = tk.Tk()
-window.title("Вайфу-синхроз-инатор")
-window.resizable(True, True) # Разрешаем масштабирование...
-
+window.title("Вайфу-синхро-инатор")
+window.resizable(True, True)
 
 button_frame = ttk.Frame(window)
 button_frame.pack(pady=10)
 
-
-connect_button = ttk.Button(button_frame, text="Подключиться к серверу", command=connect_to_server)
+connect_button = ttk.Button(button_frame, text="Подключиться", command=connect_to_server)
 connect_button.pack(side=tk.LEFT, padx=5)
 
-disconnect_button = ttk.Button(button_frame, text="Отключиться от сервера", command=disconnect_from_server)
+disconnect_button = ttk.Button(button_frame, text="Отключиться", command=disconnect_from_server)
 disconnect_button.pack(side=tk.LEFT, padx=5)
 
-
-
-status_label = ttk.Label(window, text="Не подключено")
+status_label = ttk.Label(window, text="Отсоединено")
 status_label.pack()
 
+main_frame = ttk.Frame(window)
+main_frame.pack(pady=5, fill=tk.BOTH, expand=True)
 
-list_frame = ttk.Frame(window)
-list_frame.pack(pady=5, fill=tk.BOTH, expand=True) # fill=tk.BOTH, expand=True для заполнения пространства, а то мало ли
+# Карточки
+card_frame = ttk.LabelFrame(main_frame, text="Карточки")
+card_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=5, pady=5) # Изменено side=tk.TOP
 
-list_frame.columnconfigure(0, weight=1)
-list_frame.columnconfigure(1, weight=1)
-list_frame.rowconfigure(1, weight=1) # weight=1 для listbox
+server_card_label = ttk.Label(card_frame, text="На стороне сервера:")
+server_card_label.grid(row=0, column=0, sticky=tk.W)
+
+server_card_treeview = ttk.Treeview(card_frame, columns=("Name", "Size (MB)", "Modified"), show="headings")
+server_card_treeview.heading("Name", text="Name")
+server_card_treeview.heading("Size (MB)", text="Size (MB)")
+server_card_treeview.heading("Modified", text="Modified")
+server_card_treeview.grid(row=1, column=0, sticky="nsew")
+
+download_card_button = ttk.Button(card_frame, text="Загрузить с сервера", command=lambda: download_selected("cards"))
+download_card_button.grid(row=2, column=0, pady=(5, 0))
+
+local_card_label = ttk.Label(card_frame, text="На диске:")
+local_card_label.grid(row=0, column=1, sticky=tk.W, padx=(20, 0))
+
+local_card_treeview = ttk.Treeview(card_frame, columns=("Name", "Size (MB)", "Modified"), show="headings")
+local_card_treeview.heading("Name", text="Name")
+local_card_treeview.heading("Size (MB)", text="Size (MB)")
+local_card_treeview.heading("Modified", text="Modified")
+local_card_treeview.grid(row=1, column=1, sticky="nsew", padx=(20, 0))
+
+upload_card_button = ttk.Button(card_frame, text="Выгрузить на сервер", command=lambda: upload_selected("cards"))
+upload_card_button.grid(row=2, column=1, pady=(5, 0), padx=(20, 0))
+
+# Моды
+mod_frame = ttk.LabelFrame(main_frame, text="Моды")
+mod_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=5, pady=5) # Изменено side=tk.TOP
+
+server_mod_label = ttk.Label(mod_frame, text="На стороне сервера:")
+server_mod_label.grid(row=0, column=0, sticky=tk.W)
+
+server_mod_treeview = ttk.Treeview(mod_frame, columns=("Name", "Size (MB)", "Modified"), show="headings")
+server_mod_treeview.heading("Name", text="Name")
+server_mod_treeview.heading("Size (MB)", text="Size (MB)")
+server_mod_treeview.heading("Modified", text="Modified")
+server_mod_treeview.grid(row=1, column=0, sticky="nsew")
+
+download_mod_button = ttk.Button(mod_frame, text="Загрузить с сервера", command=lambda: download_selected("mods"))
+download_mod_button.grid(row=2, column=0, pady=(5, 0))
+
+local_mod_label = ttk.Label(mod_frame, text="На диске:")
+local_mod_label.grid(row=0, column=1, sticky=tk.W, padx=(20, 0))
+
+local_mod_treeview = ttk.Treeview(mod_frame, columns=("Name", "Size (MB)", "Modified"), show="headings")
+local_mod_treeview.heading("Name", text="Name")
+local_mod_treeview.heading("Size (MB)", text="Size (MB)")
+local_mod_treeview.heading("Modified", text="Modified")
+local_mod_treeview.grid(row=1, column=1, sticky="nsew", padx=(20, 0))
+
+upload_mod_button = ttk.Button(mod_frame, text="Выгрузить на сервер", command=lambda: upload_selected("mods"))
+upload_mod_button.grid(row=2, column=1, pady=(5, 0), padx=(20, 0))
 
 
-server_file_label = ttk.Label(list_frame, text="Файлы на сервере:")
-server_file_label.grid(row=0, column=0, sticky=tk.W, padx=(100,0))
 
-server_file_treeview = ttk.Treeview(list_frame, columns=("Name", "Size (MB)", "Modified"), show="headings")
-server_file_treeview.heading("Name", text="Name")
-server_file_treeview.heading("Size (MB)", text="Size (MB)")
-server_file_treeview.heading("Modified", text="Modified")
-server_file_treeview.grid(row=1, column=0, sticky="nsew")
+main_frame.columnconfigure(0, weight=1)
+main_frame.columnconfigure(1, weight=1)
 
+card_frame.columnconfigure(0, weight=1)
+card_frame.columnconfigure(1, weight=1)
+card_frame.rowconfigure(1, weight=1)
 
-download_button = ttk.Button(list_frame, text="Скачать выделенное", command=download_selected)
-download_button.grid(row=2, column=0, pady=(5,0))
-
-
-
-local_file_label = ttk.Label(list_frame, text="Файлы на диске:")
-local_file_label.grid(row=0, column=1, sticky=tk.W, padx=(150,0))
-
-
-local_file_treeview = ttk.Treeview(list_frame, columns=("Name", "Size (MB)", "Modified"), show="headings")
-local_file_treeview.heading("Name", text="Name")
-local_file_treeview.heading("Size (MB)", text="Size (MB)")
-local_file_treeview.heading("Modified", text="Modified")
-local_file_treeview.grid(row=1, column=1, sticky="nsew", padx=(20,0))
-
-
-upload_button = ttk.Button(list_frame, text="Выгрузить выделенное на сервер", command=upload_selected)
-upload_button.grid(row=2, column=1, pady=(5, 0), padx=(20,0))
-
-
+mod_frame.columnconfigure(0, weight=1)
+mod_frame.columnconfigure(1, weight=1)
+mod_frame.rowconfigure(1, weight=1)
 
 window.mainloop()
-
-
-
-
-
-
-
-# sosal?
--- burninghellascardupdaterSRV.py
+++ burninghellascardupdaterSRV.py
@@ -5,9 +5,11 @@ import os
 import json
 import time
 
-HOST = '0.0.0.0'  # Слушаем на всех интерфейсах
-PORT = 1337  # Порт
-FOLDER = 'C:\\KKS\\UserData\\chara\\female\\burning_hellas'  # Папка с карточками
+HOST = '0.0.0.0'
+PORT = 1337
+CARD_FOLDER = 'C:\\KKS\\UserData\\chara\\female\\burning_hellas'  # Папка с карточками
+MOD_FOLDER = 'C:\\KKS\\mods' # Папка с модами
+
 
 def handle_client(conn, addr):
     print(f'Подключен клиент: {addr}')
@@ -19,12 +21,16 @@ def handle_client(conn, addr):
 
             request = json.loads(data)
             command = request.get('command')
+            folder = request.get('folder') #  Получаем имя папки
 
             if command == 'list_files':
                 try:
+                    target_folder = CARD_FOLDER if folder == 'cards' else MOD_FOLDER if folder == 'mods' else None
+                    if not target_folder:
+                        raise ValueError("Invalid folder specified")
                     files = {}
-                    for filename in os.listdir(FOLDER):
-                        filepath = os.path.join(FOLDER, filename)
+                    for filename in os.listdir(target_folder):
+                        filepath = os.path.join(target_folder, filename)
                         if os.path.isfile(filepath):
                             with open(filepath, 'rb') as f:
                                 filehash = hashlib.md5(f.read()).hexdigest()
@@ -34,9 +40,16 @@ def handle_client(conn, addr):
                     print(f"Error listing files: {e}")
                     conn.sendall(json.dumps({'error': str(e)}).encode())
 
+
             elif command == 'get_file':
                 filename = request.get('filename')
-                filepath = os.path.join(FOLDER, filename)
+                target_folder = CARD_FOLDER if folder == 'cards' else MOD_FOLDER if folder == 'mods' else None
+                if not target_folder:
+                        raise ValueError("Invalid folder specified")
+
+                filepath = os.path.join(target_folder, filename)
+
+
                 if os.path.isfile(filepath):
                     try:
                         filesize = os.path.getsize(filepath)
@@ -49,15 +62,22 @@ def handle_client(conn, addr):
 
             elif command == 'upload_file':
                 filename = request.get('filename')
-                filesize = int(request.get('size')) # Приводим к int
-                filepath = os.path.join(FOLDER, filename)
+                filesize = int(request.get('size'))
+                target_folder = CARD_FOLDER if folder == 'cards' else MOD_FOLDER if folder == 'mods' else None
+
+                if not target_folder:
+                    raise ValueError("Invalid folder specified")
+
+
+                filepath = os.path.join(target_folder, filename)
+
                 try:
                     with open(filepath, 'wb') as f:
                         received = 0
-                        while received < filesize: # Ограничиваем чтение размером файла
+                        while received < filesize:
                             data = conn.recv(4096)
                             if not data:
-                                break # Выходим, если данные не получены
+                                break
                             f.write(data)
                             received += len(data)
                 except Exception as e:
@@ -65,11 +85,11 @@ def handle_client(conn, addr):
 
 
     except Exception as e:
-        print(f'Ошибка: {e}')
+        print(f'Error: {e}')
+
     finally:
         conn.close()
         print(f'Клиент отключен: {addr}')
-
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
